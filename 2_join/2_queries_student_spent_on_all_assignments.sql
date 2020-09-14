135d6591a5fd99d21d6f565c1c058a9438a5d023
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-09-14 23:19:12 +0000

    completed 2_join_queries

--- 2_join/2_queries_student_spent_on_all_assignments.sql
+++ 2_join/2_queries_student_spent_on_all_assignments.sql
@@ -2,4 +2,3 @@ SELECT SUM(assignment_submissions.duration) as total_duration
 FROM assignment_submissions
 JOIN students ON students.id = assignment_submissions.student_id
 WHERE students.name='Ibrahim Schimmel'
-GROUP BY students.name;
